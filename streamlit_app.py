983cacc303036c19f691edc57905ec4e1a99ee97
aidookingsley <kaidoog36@gmail.com>
2025-06-27 04:18:52 +0000

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Fetch credentials from environment
-REGION = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
+REGION = os.getenv("AWS_REGION", "us-east-1")
 access_key = os.getenv("AWS_ACCESS_KEY_ID")
 secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
