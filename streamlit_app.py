48279fcac544a07c9ec04fed7808f135f96ccf18
aidookingsley <kaidoog36@gmail.com>
2025-06-27 03:58:32 +0000

    fix errors with aws client

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ load_dotenv()
 region = os.getenv("AWS_REGION")
 access_key = os.getenv("AWS_ACCESS_KEY_ID")
 secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
